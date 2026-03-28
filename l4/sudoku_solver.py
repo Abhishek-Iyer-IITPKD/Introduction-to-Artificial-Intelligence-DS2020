ad8a1d2b5d3014bdb18b763eaa14b19f90824260
Abhishek Iyer <112401002@smail.iitpkd.ac.in>
2026-03-28 11:10:41 +0530

    Tentatively done

--- l4/sudoku_solver.py
+++ l4/sudoku_solver.py
@@ -1,13 +1,26 @@
 import sys, os
-import pycosat as pc
+import pycosat
 from collections import defaultdict
 import numpy as np
+
+def var(row, col, digit):
+    return 81*row+9*col+digit
+
+def make_diff(cells):
+    for cell in cells:
+        for cell1 in cells:
+            if cell != cell1:
+                if cell[0] < cell1[0] or cell[1] < cell1[1]:
+                    for d in range(1,10):
+                        clauses.append([-var(cell[0],cell[1],d), -var(cell1[0],cell1[1],d)])
 
 if __name__=='__main__':
     input_file  = sys.argv[1]
     output_file = os.path.join(os.path.dirname(os.path.abspath(input_file)), "output.txt")
     try:
         with open(input_file, 'r') as f:
+            with open(output_file, 'w') as f1:
+                pass
             grids = f.readlines()
             
             for grid in grids:
@@ -19,55 +32,51 @@ if __name__=='__main__':
                 if len(grid) != 81:
                     raise Exception("Invalid grid size")
                 
-                board = np.zeros([9,9]).astype(np.uint8)
-                for i in range(9):
-                    for j in range(9):
-                        try:
-                            board[i,j] = int(grid[i*9+j])
-                        except ValueError:
-                            continue
-                
                 clauses = []
                 
-                # Each row
+                # Existing cells
                 for i in range(9):
                     for j in range(9):
-                        if board[i,j] == 0:
-                            clauses.append([board[i,j]])
-                            
-                # Each column
+                        if grid[9*i+j] != '.':
+                            clauses.append([var(i,j,int(grid[9*i+j]))])
+                
+                # Cells are unique
                 for i in range(9):
                     for j in range(9):
-                        if board[j,i] == 0:
-                            clauses.append([board[j,i]])
-                            
-                # Each box
+                        for d in range(1,10):
+                            for d1 in range(d+1,10):
+                                clauses.append([-var(i,j,d), -var(i,j,d1)])
+                
+                # Cells are from 1-9
+                for i in range(9):
+                    for j in range(9):
+                        clauses.append([var(i,j,d) for d in range(1,10)])
+
+                # Rows and columns are from 1-9
+                for i in range(9):
+                    for j in range(9):
+                        make_diff([[i, j] for j in range(9)])
+                        make_diff([[j, i] for j in range(9)])
+                
+                # 3x3 boxes are from 1-9
                 for i in range(3):
                     for j in range(3):
-                        for k in range(3):
-                            if board[3*i+j,3*k+i] == 0:
-                                clauses.append([board[3*i+j,3*k+i]])
-                                
-                # Each number
-                for i in range(1,10):
-                    for j in range(1,10):
-                        for k in range(1,10):
-                            for l in range(1,10):
-                                if board[i-1,j-1] == i and board[j-1,k-1] == j and board[k-1,l-1] == k and board[l-1,i-1] == l:
-                                    clauses.append([board[i-1,j-1], board[j-1,k-1], board[k-1,l-1], board[l-1,i-1]])
-                                    
-                clauses.append([-board[i,j] for i in range(9) for j in range(9)])
+                        make_diff([[3*i+k%3, 3*j+k//3] for k in range(9)])
                 
-                model = pc.solve()
-                solution = np.zeros([9,9]).astype(np.uint8)
-                for i in range(9):
-                    for j in range(9):
-                        solution[i,j] = model.value(board[i,j])
+                solution = pycosat.solve(clauses)
+                # for i in range(9):
+                #     for j in range(9):
+                #         for d in range(1,10):
+                #             if var(i,j,d) in solution:
+                #                 print(d,end="")
+                # print()
                         
                 with open(output_file, 'a') as f:
                     for i in range(9):
                         for j in range(9):
-                            f.write(str(solution[i,j]))
-                        f.write("\n")
+                            for d in range(1,10):
+                                if var(i,j,d) in solution:
+                                    f.write(str(d))
+                    f.write("\n")
     except FileNotFoundError:
         print("Error: The file was not found.")
